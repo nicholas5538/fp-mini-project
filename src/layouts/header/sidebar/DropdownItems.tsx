cc39f16fc0dfb7ec8b92f4eda796c2f85fea237e
Nicholas Yong <n.yong@yy4c4yq2yr.ad.deliveryhero.com>
2023-01-28 14:47:55 +0800

    Change conditional rendering codes for dropdown menu

--- src/layouts/header/sidebar/DropdownItems.tsx
+++ src/layouts/header/sidebar/DropdownItems.tsx
@@ -2,28 +2,22 @@ import React, { useId } from 'react';
 import { NavLink } from 'react-router-dom';
 import { dropdownObj } from '../../../constants/listObj';
 
-type hiddenStatus = {
-    isHidden: boolean;
-};
+const DropdownItems = () => {
+    const dropdownElements = dropdownObj.map((obj) => (
+        <li key={useId()}>
+            <NavLink
+                to={obj.path}
+                className={({ isActive }) =>
+                    isActive ? 'route-link-active' : 'route-link'
+                }
+            >
+                <obj.svgElement className='md:hidden lg:block' />
+                <span className='route-text'>{obj.text}</span>
+            </NavLink>
+        </li>
+    ));
 
-const DropdownItems = ({ isHidden }: hiddenStatus) => {
-    return !isHidden ? (
-        <>
-            {dropdownObj.map((obj) => (
-                <li key={useId()}>
-                    <NavLink
-                        to={obj.path}
-                        className={({ isActive }) =>
-                            isActive ? 'route-link-active' : 'route-link'
-                        }
-                    >
-                        <obj.svgElement className='md:hidden lg:block' />
-                        <span className='route-text'>{obj.text}</span>
-                    </NavLink>
-                </li>
-            ))}
-        </>
-    ) : null;
+    return <>{dropdownElements}</>;
 };
 
 export default DropdownItems;
